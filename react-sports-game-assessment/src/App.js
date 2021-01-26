4b12b4b5d56703f2dfc247f78875728f614f0bcf
Karina Vazquez <karina.vazquez6@yahoo.com>
2021-01-26 13:21:05 -0600

    first commit

---
+++ react-sports-game-assessment/src/App.js
@@ -0,0 +1,39 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Game from "./Components/Game/Game";
+
+function App(props) {
+  const raccoons = {
+    name: "Russiaville Raccoons",
+    logoSrc: "./assets/images/raccoon.png",
+  };
+
+  const squirrels = {
+    name: "Sheridan Squirrels",
+    logoSrc: "./assets/images/squirrel.png",
+  };
+
+  const bunnies = {
+    name: "Burlington Bunnies",
+    logoSrc: "./assets/images/bunny.png",
+  };
+
+  const hounds = {
+    name: "Hammond Hounds",
+    logoSrc: "./assets/images/hound.png",
+  };
+
+  return (
+    <div className="App">
+      <Game
+        venue="Union 525 Gem"
+        homeTeam={squirrels}
+        visitingTeam={raccoons}
+      />
+      <Game venue="Sheridan Arena" homeTeam={bunnies} visitingTeam={hounds} />
+    </div>
+  );
+}
+
+export default App;
